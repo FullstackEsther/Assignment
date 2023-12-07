d261f445bd1b53b9853e4d78902c444c4b9bc4f0
FullstackEsther <otufaleesther@gmail.com>
2023-12-07 15:35:14 +0100

    commit two

---
+++ Assignment.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    public class Assignment
+    {
+        public string Age {get;set;}
+     public string Age1 {get;set;}
+      public string Age2 {get;set;}
+      public string Ageg {get;set;}
+    }
+}
